abda5bb4fa63843f12142a3d70198f32b58fc49c
Dinh Duc Thuan <80194045+thuandd2002@users.noreply.github.com>
2024-01-03 15:33:39 +0700

    fix

--- app/Models/Colors.php
+++ app/Models/Colors.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Colors extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $table = "colors";
     protected $fillable = ['id', 'product_id', 'name'];
     public function products()
--- database/migrations/2023_12_21_063411_create_categories_table.php
+++ database/migrations/2023_12_21_063411_create_categories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('images')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
