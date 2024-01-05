5fda24c6e21673b69e81ef0db7a59c399eeb5c76
Dinh Duc Thuan <80194045+thuandd2002@users.noreply.github.com>
2024-01-05 12:08:36 +0700

    fix

--- resources/views/admin/layout/slidebar.blade.php
+++ resources/views/admin/layout/slidebar.blade.php
@@ -36,15 +36,6 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-         
-          <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Widgets
-              </p>
-            </a>
-          </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
