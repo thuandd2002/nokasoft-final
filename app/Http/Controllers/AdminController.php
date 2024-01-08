a3dd3e8e38ea2e036b1fbd2a4e4b07fcc58947de
thuandd2002 <dinhducthuan2002@gmail.com>
2024-01-08 14:46:21 +0700

    fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,17 +11,14 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+
 class AdminController extends Controller
 {
     function admin()
     {
-        if (Auth::check()) {
-            return view('admin.layout.layout');
-        } else {
-            return view('admin.auth.login');
-        }
-     
+        return Auth::check() ? view('admin.layout.layout') : view('admin.auth.login');
     }
+    
     function login()
     {
         return view('admin.auth.login');
@@ -44,7 +41,7 @@ class AdminController extends Controller
         } else {
             $email = $request->input('email');
             $password = $request->input('password');
-            if (Auth::attempt(['email' => $email, 'password' => $password, 'roles'=>1])) {
+            if (Auth::attempt(['email' => $email, 'password' => $password, 'roles' => 1])) {
                 return redirect('admin');
             } else {
                 Session::flash('error', 'Email hoặc mật khẩu k đúng');
@@ -72,28 +69,28 @@ class AdminController extends Controller
             if ($validator->fails()) {
                 return redirect('route_admin_forgot_password')->withErrors($validator);
             } else {
-               
+
                 $user = User::where('email', $request->input('email'))->first();
                 $token = strtoupper(Str::random(6));
                 $user->update(['remember_token' => $token]);
                 $user->update(['name' => 'abc']);
                 Mail::send('emails.forgot-password', compact('user'), function ($email) use ($user) {
-                   
+
                     $email->subject('Noka - Password Reset');
-                  
+
                     $email->to($user->email, $user->name);
-                  
                 });
-                Session::flash('success', 'Please check the email to reset password');  
+                Session::flash('success', 'Please check the email to reset password');
                 return redirect()->route('route_admin_forgot_password');
             }
         }
         return view('admin.auth.forgot-password');
     }
-    
-    function changePassword(User $user, Request $request, $id, $remember_token) {
-        $user = User::find($id);     
-        if ($request->isMethod('POST')){
+
+    function changePassword(User $user, Request $request, $id, $remember_token)
+    {
+        $user = User::find($id);
+        if ($request->isMethod('POST')) {
             $rules = [
                 'password' => 'required',
                 'confirmPassword' => 'required|same:password',
@@ -104,29 +101,30 @@ class AdminController extends Controller
             ];
             $validator = Validator::make($request->all(), $rules, $messages);
             if ($validator->fails()) {
-                return redirect()->route('route_admin_change_password',['id'=>$user->id, 'remember_token'=>$user->remember_token])->withErrors($validator);
-            }else{
+                return redirect()->route('route_admin_change_password', ['id' => $user->id, 'remember_token' => $user->remember_token])->withErrors($validator);
+            } else {
                 $password = $request->input('password');
                 $password_h = bcrypt($password);
                 $user->update(['password' => $password_h, 'remember_token' => null]);
-                Session::flash('success', 'Password change was successful');  
+                Session::flash('success', 'Password change was successful');
                 return redirect()->route('admin_login');
             }
         }
-        if($remember_token === $user->remember_token) {
-            return view('admin.auth.change-password',['user' => $user]);
-        }else{
+        if ($remember_token === $user->remember_token) {
+            return view('admin.auth.change-password', ['user' => $user]);
+        } else {
             return abort(404);
-        }   
+        }
     }
 
-    function register(Request $request){
-        if($request->isMethod('POST')){
-          
-           
+    function register(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+
+
             $rules = [
-                'name'=> 'required',
-                'email'=> 'required|email|unique:users',
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
                 'password' => 'required',
                 'repassword' => 'required|same:password',
             ];
@@ -139,21 +137,20 @@ class AdminController extends Controller
             $validator = Validator::make($request->all(), $rules, $messages);
             if ($validator->fails()) {
                 return redirect('route_admin_register')->withErrors($validator);
-            }else{
-                
+            } else {
+
                 $params = [];
                 $params['cols'] = $request->post();
-                $params['cols']['roles']= 1;
+                $params['cols']['roles'] = 1;
                 unset($params['cols']['_token']);
                 unset($params['cols']['repassword']);
-                $params['cols']['password']= Hash::make($params['cols']['password']);
+                $params['cols']['password'] = Hash::make($params['cols']['password']);
                 DB::table('users')->insert($params);
                 Session::flash('success', 'User register successfully');
                 return redirect()->route('admin_login');
             }
-
         }
-        
+
         return view('admin.auth.register');
     }
 }
